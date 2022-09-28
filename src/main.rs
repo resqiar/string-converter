f2999ce372bea6956440724bdce3668b33fe23fe
resqiar <resqiageng@gmail.com>
2022-09-29 02:31:29 +0700

    use references instead of move

--- src/main.rs
+++ src/main.rs
@@ -14,24 +14,25 @@ fn main() {
     // Get the index of 1 in the args, why 1 not 0?
     // because the first args is reserved for the program's binary name.
     // see: https://doc.rust-lang.org/book/ch12-01-accepting-command-line-arguments.html#reading-the-argument-values
-    let file_path = &args[1];
+    let file_path: &String = &args[1];
 
     // read the file and convert them into a single string
-    let raw_str: String = read_file(file_path.to_string());
+    let raw_str: String = read_file(&file_path.to_string());
 
     // format with random capital character
-    let formatted_str = convert_to_random_uppercase(&raw_str);
+    let formatted_str: String = convert_to_random_uppercase(&raw_str);
 
     println!("{}", formatted_str);
 }
 
-fn read_file(file_path: String) -> String {
+fn read_file(file_path: &String) -> String {
     let file_content: String = fs::read_to_string(file_path).expect("File not found");
     return file_content;
 }
 
-fn convert_to_random_uppercase(input: &str) -> String {
-    let mut converted_str = String::new();
+fn convert_to_random_uppercase(input: &String) -> String {
+    // Stored on the heap (dynamic size)
+    let mut converted_str: String = String::new();
 
     for char in input.chars() {
         // If the char for current iteration is not an alphabetic,
@@ -55,7 +56,7 @@ fn convert_to_random_uppercase(input: &str) -> String {
         else {
             converted_str.push(char.to_ascii_uppercase());
         }
-    }
+    } // converted_str is auto-dropped by rust here
 
     // Return the converted_str
     return converted_str;
